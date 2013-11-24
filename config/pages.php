1f8cd4fa8763e1f0a1dd20634574ac2ab07ffa24
dwise006 <dwise006@odu.edu>
2013-11-24 12:19:33 -0500

    Added back The most recent changes

---
+++ config/pages.php
@@ -0,0 +1,45 @@
+<?php
+/*
+All pages you have in the /pages/ folder must be defined here, it's their own title (shown on top and in the url)
+
+ex: $pageTitles['file.php'] = "This is the title";
+
+Just copy paste this and fill in :
+
+$pageTitles[''] = "";
+
+/* PAGE OPTIONS */
+
+$pageTitles['admin/admins.php'] = "Administration";
+$pageTitles['admin/accountmaint.php'] = "Account";
+$pageTitles['user/welcome.php'] = "Welcome";
+$pageTitles['user/members.php'] = "Members";
+$pageTitles['creation/new_campaign.php'] = "NewCampaign";
+$pageTitles['help/about.php'] = "About";
+$pageTitles['help/help.php'] = "Help";
+$pageTitles['editaccount.php'] = "Edituser";
+$pageTitles['user/myaccount.php'] = "MyAccount";
+$pageTitles['admin/cdnsetup.php'] = "CDN Setup";
+$pageTitles['content/content.php'] = "Content";
+$pageTitles['content/mycontent.php'] = "My Content";
+$pageTitles['email/email.php'] = "Email";
+$pageTitles['email/myemail.php'] = "My Email";
+$pageTitles['campaigns/campaigns.php'] = "Campaigns";
+$pageTitles['search/search.php'] = "Search";
+$pageTitles['search/searchCampaign.php'] = "SearchCampaign";
+$pageTitles['search/searchContent.php'] = "SearchContent";
+$pageTitles['search/searchEmail.php'] = "SearchEmail";
+$pageTitles['admin/siteconfig.php'] = "SiteConfig";
+$pageTitles['workflow/reviewtasks.php'] = "ReviewTasks";
+$pageTitles['workflow/mytasks.php'] = "MyTasks";
+$pageTitles['workflow/addedtoday.php'] = "AllTasks";
+
+
+
+
+
+$pageDescriptions['welcome.php'] = "This is the welcome page. Welcome.";
+$pageDescriptions['members.php'] = "This is the members page. Welcome.";
+
+$pageKeywords['welcome.php'] = "welcome, page, test";
+?>
---
+++ pages/help/about.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>About EmVi</title>
+<style type="text/css">>
+body {
+    margin: 10px;
+    background-color:#FF9933;
+}
+
+#leftcolumn{
+    float: left;
+    width: 250px;
+    background-color:#FF9933;
+
+}
+#rightcolumn{
+   color: black;
+   background-color: #FF9933;
+
+</style>
+</head>
+
+<body>
+  <script type="text/javascript">
+    
+    </script>
+<!--  <div id="wrapper">
+    <div id="leftcolumn">
+      <a href="index.html">Index</a>
+      <a href="Jailbreaking.html">Jailbreaking</a>
+      <a href="Rooting.html">Rooting</a>
+      <a href="Graphic_Design.html">Graphic Design</a>
+      <a href="Sources.html">Sources</a>
+      <a href="Contact.html">Contact</a>
+      <a href="MyCSS_Styles.html">MyCSS_Styles</a>
+      <a href="MyJavaScript.html">MyJavaScript</a>
+      </div>-->
+    <div id="rightcolumn">
+
+
+ <div id="logo"> 
+   <h1><p style="color:#000000;background-color:#FF9933">
+   EmVi</br>The Competition will be EmVious!<h1>
+      </div>
+      <div class="content">
+	<p style="background-color:#FF9933">
+<p>EmVi can be used as a stand-alone system or integrated with other software through the use of an Application Programming Interface(API).  It is a global solution for email marketing content management, campaign distibution, and testing.</a></p>
+
+
+<h1>Key Facts & Figures</h1>
+<ul>
+<li>87% of email users check their personal email accounts at least once daily.</li>
+<li>Consumers report that about 2/3 of the email they received were marketing (non-SPAM) messages.</li>
+<li>Consumers report that about 2/3 of the email they received were marketing (non-SPAM) messages.</li>
+<li>30% of non-business email engagement time is devoted to commercial email.</li>
+<li>Consumers overwhelmingly prefer email for commercial communications over any other mode.</li>
+<li>43% of Internet-enabled phone owners use their phone to check email 4+ times/day. Email is the most wide-spread online mobile activity.</li><br></ul>
+
+<h1>What We Do</h1>
+
+<h2>Content Management</h2>
+<li>Ability to store, edit the plain text html, and search within set parameters of the campaign.</li>
+<h2>Upload to CDN</h2>
+<li>Uploads images to Content Delivery Network and automatically replaces the image url in the HTML.</li>
+<h2>Approval System</h2>
+<li> Provides a customizable approval system that allows campaigns to have multiple stages of approval.</li>
+
+<h2>Contact Us</h2>
+<li>email: EmViTeam@yahoo.com</li>
+<li>twitter: @TeamEmVi</li>
+ 
+<h2><a href="http://www.cs.odu.edu/~411orang">Explore EmVi!</a></h2>
+</div>
+<div id="footer">
+   Copyright &copy; 2013 EmVi Team Orange<br/>
+    Last Updated on 11/23/13
+   </div>
+</div>
+</div>
+ </body>
+ </html>
